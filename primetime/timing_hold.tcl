80253137dcbfd490ea521e3a5d06924dd1e9c429
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-05-18 15:19:38 +0300

    Fix hold timing report names

--- primetime/timing_hold.tcl
+++ primetime/timing_hold.tcl
@@ -4,8 +4,8 @@ set link_library "fast.db"
 link_design
 source "../timing/postlayout.sdc"
 read_parasitics "../timing/serial_peripheral_interface.spef"
-report_constraint -all_violators > "./reports/setup_viol.rpt"
+report_constraint -all_violators > "./reports/hold_viol.rpt"
 report_timing -delay_type max -max_paths 10 \
--slack_lesser_than 10 > "./reports/setup_check.rpt"
+-slack_lesser_than 10 > "./reports/hold_check.rpt"
 set power_enable_analysis true
-report_power -verbose > "./reports/setup_power.rpt"
+report_power -verbose > "./reports/hold_power.rpt"
